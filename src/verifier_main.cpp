cec9ceabedaca28c536aae652b4e3c5af87cf22c
piobpl <piotrb10@gmail.com>
2016-03-21 11:06:51 +0100

    Verifier fix

--- src/verifier_main.cpp
+++ src/verifier_main.cpp
@@ -8,22 +8,12 @@
 #include "../src/output.h"
 #include "../src/model.h"
 
-FILE* testfile = nullptr;
-FILE* ansfile = nullptr;
-
-int n;
-std::vector<Clause> clauses;
-Model model;
-
-bool unsatisfiable(const Clause &clause) {
-  return !model.satisfied(clause);
-}
-
-int verify() {
-  Input::read(testfile, &n, &clauses);
-  Output::read(ansfile, n, &model);
+int verify(const Model &model, const std::vector<Clause> clauses) {
   auto unsatisfiedCount =
-    std::count_if(clauses.begin(), clauses.end(), unsatisfiable);
+    std::count_if(
+      clauses.begin(),
+      clauses.end(),
+      [&model](const Clause &c){ return !model.satisfied(c); });
   if (unsatisfiedCount > 0) {
     std::cerr << unsatisfiedCount << " unsatisfied clauses\n";
     return -1;
@@ -32,6 +22,13 @@ int verify() {
 }
 
 int main(int argc, char *argv[]) {
+  FILE* testfile = nullptr;
+  FILE* ansfile = nullptr;
+
+  int n;
+  std::vector<Clause> clauses;
+  Model model;
+
   if (argc == 2) {
     testfile = fopen(argv[1], "r");
     ansfile = stdin;
@@ -42,8 +39,14 @@ int main(int argc, char *argv[]) {
     std::cerr << "!Usage: verifier TEST [ANS]\n";
     exit(-1);
   }
-  int exitCode = verify();
+
+  Input::read(testfile, &n, &clauses);
+  Output::read(ansfile, n, &model);
+
+  int exitCode = verify(model, clauses);
+
   fclose(testfile);
   fclose(ansfile);
+
   return exitCode;
 }
